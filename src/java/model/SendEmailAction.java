30a91f13f5bca519d40d3cd30666fe08b4f8b24e
GIGOTech <tompreciousekwere@outlook.com>
2021-08-23 10:14:05 +0100

    Database Commit

--- src/java/model/SendEmailAction.java
+++ src/java/model/SendEmailAction.java
@@ -44,20 +44,22 @@ public class SendEmailAction extends HttpServlet {
             String entryDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
             
             
-            request.getRequestDispatcher("/response.jsp").forward(request, response);
+//            request.getRequestDispatcher("/response.jsp").forward(request, response);
 //            out.print(name);
 //            out.print(email);
 //            out.print(subject);
 //            out.print(message);
 
-//            RegisterBean bean = new RegisterBean(0,email,name,subject,message,entryDate);
-//            int status = new RegisterDAO().addUser(bean);
-//            
-//            if(status == 0){
-//                request.getRequestDispatcher("/response.jsp").forward(request, response);
-//            }else{
-//                request.getRequestDispatcher("/index.jsp").forward(request, response);
-//            }
+            RegisterBean bean = new RegisterBean(0,email,name,subject,message,entryDate);
+            int status = new RegisterDAO().addUser(bean);
+            
+            if(status == 0){
+                request.getRequestDispatcher("/response.jsp").forward(request, response);
+            }else{
+                request.getRequestDispatcher("/index.jsp").forward(request, response);
+            }
+        }catch(Exception ex){
+            ex.printStackTrace();
         }
     }
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -98,5 +100,4 @@ public class SendEmailAction extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
 }
